ffb968bc3f6ffe553ec5604766443f42ef0d64b6
Jonathon Silva Eng <54660634+jonathon-eng@users.noreply.github.com>
2022-10-01 07:19:56 -0700

    Cleaning up

--- script.js
+++ script.js
@@ -1,73 +1,3 @@
-// const form = document.querySelector("form");
-// // const submit = document.querySelector("submit")
-
-// form.addEventListener('invalid', e => {
-   
-//       e.preventDefault();
-
-//       check();
-    
-// });
-
-// function check () {
-//     // (C1) INIT
-//     let valid = true, error = "", field = "";
-
-//      // (C2) NAME
-//     field = document.getElementById("Fname");
-//     error = document.getElementById("cname");
-//     if (!field.checkValidity()) {
-//       valid = false;
-//       field.classList.add("err");
-//       error.innerHTML = "First Name cannot be empty\r\n";
-//       return;
-//     } else {
-//       field.classList.remove("err");
-//       error.innerHTML = "";
-//     }
-
-//     // // (C3) LAST NAME
-//     // field = document.getElementById("Lname");
-//     // error = document.getElementById("clname");
-//     // if (!field.checkValidity()) {
-//     //     valid = false;
-//     //     field.classList.add("err");
-//     //     error.innerHTML = "Last Name cannot be empty\r\n";
-//     // } else {
-//     //     field.classList.remove("err");
-//     //     error.innerHTML = "";
-//     // }
-    
-//     // // (C4) EMAIL
-//     // field = document.getElementById("Email");
-//     // error = document.getElementById("email");
-//     // if (!field.checkValidity()) {
-//     //     valid = false;
-//     //     field.classList.add("err");
-//     //     error.innerHTML = "Looks like this is not an email\r\n";
-//     // } else {
-//     //     field.classList.remove("err");
-//     //     error.innerHTML = "";
-//     // }
-    
-//     // // (C5) PASSWORD
-//     // field = document.getElementById("Pass");
-//     // error = document.getElementById("password");
-//     // if (!field.checkValidity()) {
-//     //   valid = false;
-//     //   field.classList.add("err");
-//     //   error.innerHTML = "Password cannot be empty\r\n";
-//     // } else {
-//     //   field.classList.remove("err");
-//     //   error.innerHTML = "";
-//     // }
-  
-//     event.preventDefault()
-    
-//     return valid;
-    
-//     // (C4) RESULT
-//   }
 
 function errors(){
 
@@ -150,16 +80,6 @@ function errors(){
 
 const form = document.querySelector("form");
 
-
-//     if (!field.checkValidity()) {
-//       valid = false;
-//       field.classList.add("err");
-//       error.innerHTML = "First Name cannot be empty\r\n";
-//       return;
-//     } else {
-//       field.classList.remove("err");
-//       error.innerHTML = "";
-//     }
 
 
 form.addEventListener('submit', function(event) {
